7213c2cbe3d50829d7f59a531726b9a580a33c70
shalabh-1 <shalabhbhatiya@gmail.com>
2024-03-30 22:44:39 +0530

    Good

---
+++ Java script/Second.js
@@ -0,0 +1,20 @@
+
+
+document.body.style.background="Pink";
+document.title="My Name is Shalabh";
+var x=prompt("Please Enter Your Value");
+
+if(x==1){
+    console.log("Hello" ," ",typeof x);
+}else{
+    console.log("Not Hello");    
+}
+
+
+var check=confirm("You wanna Destroy your  Laptop")
+
+if(check){
+    console.log("Your laptop is destroyed Now")
+}else{
+    console.log("Not Destroyed")
+}
